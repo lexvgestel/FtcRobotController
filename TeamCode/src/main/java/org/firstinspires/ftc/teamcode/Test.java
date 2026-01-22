a0a5c052558ec1e2a0a79e5350c023e4e3e7095c
lexvgestel <lex.v.gestel@gmail.com>
2026-01-22 22:03:36 +0100

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
-public class Test extends LinearOpMode {
-final DcMotor ALJKF = null;
-
+public class Test {
+    private DcMotor frontLeftDrive = null;
 }
